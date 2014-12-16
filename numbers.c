cf2ad09caadcf5ccefb94c29aeabba31829703b7
Tom Beckmann <tom.beckmann@student.hpi.de>
2014-12-16 13:47:50 +0100

    Reformat numbers.c, add min and avg detection

--- numbers.c
+++ numbers.c
@@ -1,12 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
-	int counter = 0;
-	int numbers[100];
-	int result = 0;
-	for(;counter!=argc;counter++){
-		result += atoi(argv[counter]);
+int main (int argc, char **argv)
+{
+	int counter, sum, min, num;
+
+	if (argc < 2) {
+		fprintf(stderr, "No numbers given, aborting.");
+		return 1;
 	}
-	printf("result: %d\n",result);
+
+	sum = min = atoi(argv[1]);
+
+	for (counter = 2; counter != argc; counter++) {
+		num = atoi(argv[counter]);
+
+		sum += num;
+
+		if (num < min)
+			min = num;
+	}
+
+	printf("Sum: %d\nAvg: %lf\nMin: %d\n", sum, (double) sum / (argc - 1), min);
 }
+
