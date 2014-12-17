d30299cc2974914c94d931dc49a5973e460da9d7
Tom Beckmann <tom.beckmann@student.hpi.de>
2014-12-17 23:21:49 +0100

    Some more cleanup, add voice 3

--- ChristmasMelody/ChristmasMelody.ino
+++ ChristmasMelody/ChristmasMelody.ino
@@ -1,4 +1,4 @@
-#include "pachelbel2_fixed.h"
+#include "pachelbel3.h"
 
 int const melody[] = MELODY;
 #ifdef DURATION
@@ -12,13 +12,6 @@ int const duration[] = DURATION;
 #define MEASURE_DURATION 3000
 // set to non-zero value to start playing after the given measure offset
 #define CANON_MEASURE_OFFSET 0
-
-void waitForTap() {
- while (analogRead(0) <= 10)
-   ;
-}
-
-/********************************************************/
 
 void note(int freq, int fraction) {
   pinMode(LOUDSPEAKER_PIN, OUTPUT);
@@ -34,23 +27,20 @@ void note(int freq, int fraction) {
   pinMode(LOUDSPEAKER_PIN, INPUT);
 }
 
-/* the setup routine runs once when you press reset: */
 void setup() {
-  // Serial.begin(9600);
   pinMode(LOUDSPEAKER_PIN, INPUT);
 }
 
-/* the loop routine runs over and over again forever: */
 void loop() {
   int i, d;
 
   for (i = 0; melody[i]; i++) {
     d = MEASURE_DURATION / duration[i];
     if (melody[i] == PAUSE) {
-      //play(NOTE_A6, 500);
       delay(d);
-    } else
+    } else {
       note(melody[i], duration[i]);
+    }
   }
  
   delay(500000);
